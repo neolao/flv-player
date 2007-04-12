41f3cbceae64d80ecf88ef5f5c2a0a6eae5ef57f
neolao <neolao@4c8ee7da-c82d-0410-a7aa-c7403dfc0a9e>
2007-04-12 15:17:41 +0000

    php stream

---
+++ flv_stream.php
@@ -0,0 +1,26 @@
+<?php
+session_cache_limiter('nocache');
+header('Expires: Thu, 19 Nov 1981 08:52:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+header('Pragma: no-cache');
+	
+$position = $_GET['position'];
+$filename = dirname(__FILE__).'/'.htmlspecialchars($_GET['file']);
+
+
+if (file_exists($filename)) {
+	header('Content-Type: video/x-flv');
+	if ($position != 0) {
+		echo 'FLV', pack('CCNN', 1, 1, 9, 9);
+	}
+	$file = fopen($filename, "rb");
+	fseek($file, $position);
+	while (!feof($file)) {
+		echo fread($file, 16384);
+		//echo fread($file, filesize($filename)); 
+	}
+	fclose($file);
+} else {
+	echo 'The file does not exist';
+}
+?>
